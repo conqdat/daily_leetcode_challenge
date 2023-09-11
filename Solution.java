10fc8001571880bd04f3bdc5ccaeb40fb1fef32f
DatTran <trancongdat.datct@gmail.com>
2023-09-11 20:58:38 +0700

    add

--- Solution.java
+++ Solution.java
@@ -1,26 +1,30 @@
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 class Solution {
 
-    public static int findMaxConsecutiveOnes(int[] nums) {
-        int max = 0;
-        int count = 0;
-
+    public static int[] sortedSquares(int[] nums) {
         for(int i = 0; i < nums.length; i++) {
-            if(nums[i] == 1) {
-                count ++;
-            } else {
-                max = Math.max(max, count);
-                count = 0;
-            }
+            nums[i] = nums[i] * nums[i];
         }
-        return Math.max(max, count);
+        int[] sortedArray = sortIntArray(nums);
+
+        System.out.println( Arrays.toString(sortedArray) );
+        return sortedArray;
     }
+    private static int[] sortIntArray(int[] nums) {
+        int[] sortedArray = nums.clone();
+        Arrays.sort(sortedArray);
+        return sortedArray;
+    }
+
+
     public static void main(String[] args) {
-        int[] nums = new int[]{ 1,1,0,1,1,1 };
-        System.out.println( findMaxConsecutiveOnes(nums) );
+        int[] nums = new int[]{ -7,-3,2,3,11 };
+        System.out.println( sortedSquares(nums) );
     }
 }
