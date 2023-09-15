529934fb06ebb5f3a570f5ff2989282f45737402
DatTran <trancongdat.datct@gmail.com>
2023-09-15 23:56:29 +0700

    add

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Daily Leetcode Challenge
-[88. Merge Sorted Array](https://leetcode.com/problems/merge-sorted-array/).
+[27. Remove Element](https://leetcode.com/problems/remove-element/).
--- Solution.java
+++ Solution.java
@@ -8,37 +8,13 @@ import java.util.Map;
 
 class Solution {
 
-    public static void merge_1(int[] nums1, int m, int[] nums2, int n) {
-        for (int j = 0, i = m; j < n; j++) {
-            nums1[i] = nums2[j];
-            i++;
-        }
-        Arrays.sort(nums1);
-       System.out.println(Arrays.toString(nums1));
-    }
-
-    public void merge(int[] nums1, int m, int[] nums2, int n) {
-        int i = m - 1;
-        int j = n - 1;
-        int k = m + n - 1;
+    public int removeElement (int[] nums, int val) {
         
-        while (j >= 0) {
-            if (i >= 0 && nums1[i] > nums2[j]) {
-                nums1[k--] = nums1[i--];
-            } else {
-                nums1[k--] = nums2[j--];
-            }
-        }
     }
 
 
     public static void main(String[] args) {
-        int[] nums1 = new int[]{ 1,2,3,0,0,0 };
-        int[] nums2 = new int[]{ 2,5,6 };
-        int m = 3;
-        int n = 3;
 
-        merge(nums1, m, nums2, n);
 
     }
 }
