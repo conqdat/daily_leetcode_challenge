874ce18d196846d6ba92ad865f8e0bb199415af5
DatTran <trancongdat.datct@gmail.com>
2023-09-16 23:46:11 +0700

    add

--- Solution.java
+++ Solution.java
@@ -8,13 +8,21 @@ import java.util.Map;
 
 class Solution {
 
-    public int removeElement (int[] nums, int val) {
-        
+    public static int removeElement(int[] nums, int val) {
+        int index = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != val) {
+                nums[index] = nums[i];
+                index++;
+            }
+        }
+        return index;
     }
 
 
     public static void main(String[] args) {
-
-
+        int[] nums = {3,2,2,3};
+        int val = 3;
+        System.out.println( removeElement(nums, val) );
     }
 }
