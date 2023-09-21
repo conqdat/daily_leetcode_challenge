4a798c5369f6d2a323e2bd4bc73f17db1bcde222
Dat Tran <khiemtran21012000@gmail.com>
2023-09-21 13:21:50 +0700

    add

--- Solution.java
+++ Solution.java
@@ -1,52 +1,34 @@
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 class Solution {
 
-    public static void moveZeroes(int[] nums) {
-        // base case 
-        int n = nums.length;
-        if (n < 2) return;
-
-        // define pointers
-        int L = 0, R = 1;
-
-        // Move zero to the back
-        while(R < n) {
-            if(nums[L] != 0) {
-                L++;
-                R++;
-            } else if(nums[R] == 0) {
-                R++;
-            } else {
-                int temp = nums[L];
-                nums[L] = nums[R];
-                nums[R] = temp;
-            }
-        }
-
-        System.out.println( Arrays.toString(nums) );
-    }
-
-
     public static int removeDuplicates(int[] nums) {
-        int n = nums.length;
-        int L = 0;
-        int R = 1;
-        
-        while(R < n) {
-            if(nums[L] == nums[R]) {
-                R++;
-            } else if(nums[L] != nums[R]) {
-                nums[++L] = nums[R];
+        int result = 0;
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for(int i = 0; i < nums.length; i++) {
+            map.put( nums[i], map.getOrDefault(nums[i], 0) + 1);
+        }
+        System.out.println(map);
+        for(int i: map.keySet()) {
+            System.out.println(i);
+            if(map.get(i) > 2) {
+                result += 2;
+            } else {
+                result += map.get(i);
             }
         }
 
-        return L + 1;
+
+        return result;
     }
 
     public static void main(String[] args) {
-        int[] arr = { 0,0,1,1,1,2,2,3,3,4 };
+        int[] arr = { 0,0,1,1,1,1,2,3,3 };
 
         System.out.println( removeDuplicates(arr) );
+
     }
 }
