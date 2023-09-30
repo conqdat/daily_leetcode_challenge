c7e839ed842d010b537ae52f3e6c87a7698f9986
DatTran <trancongdat.datct@gmail.com>
2023-09-30 13:21:53 +0700

    add code

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Daily Leetcode Challenge
-[151. Reverse Words in a String](https://leetcode.com/problems/reverse-words-in-a-string).
+[238. Product of Array Except Self](https://leetcode.com/problems/product-of-array-except-self).
--- Solution.java
+++ Solution.java
@@ -1,24 +1,42 @@
+import java.util.Arrays;
 
 class Solution {
 
-    public static String reverseWords(String s) {
-        s = s.trim();
-        String[] sChar =  s.split(" ");
-        int resultIndex = 0;
-        String[] result = new String[sChar.length];
+    public static int[] productExceptSelf(int[] nums) {
+        int n = nums.length;
+        int[] leftProducts = new int[n];
+        int[] rightProducts = new int[n];
+        int[] result = new int[n];
 
-        for(int i = result.length - 1; i >= 0; i--) {
-            result[resultIndex] = sChar[i];
-            resultIndex++;
+        // Compute the product of all elements to the left of each element
+        int leftProduct = 1;
+        for (int i = 0; i < n; i++) {
+            leftProducts[i] = leftProduct;
+            leftProduct *= nums[i];
         }
-        
-        return String.join(" ", result).replaceAll("\\s+"," ");
+
+        // Compute the product of all elements to the right of each element
+        int rightProduct = 1;
+        for (int i = n - 1; i >= 0; i--) {
+            rightProducts[i] = rightProduct;
+            rightProduct *= nums[i];
+        }
+
+        // Calculate the final result
+        for (int i = 0; i < n; i++) {
+            result[i] = leftProducts[i] * rightProducts[i];
+        }
+
+
+        System.out.println( Arrays.toString(result) );
+        return result;
     }
 
+
     public static void main(String[] args) {
-        String s = "a good   example";
+        int[] nums = {1,2,3,4};
         
-        System.out.println( reverseWords(s) );
+        System.out.println( productExceptSelf(nums) );
 
     }
 }
