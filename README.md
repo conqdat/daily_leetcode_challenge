f6bf2cd2e5c111c400e6c18fadffc77e2a14a5e2
Dat Tran <khiemtran21012000@gmail.com>
2023-10-09 09:56:48 +0700

    add code

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Daily Leetcode Challenge
-[1679. Max Number of K-Sum Pairs](https://leetcode.com/problems/max-number-of-k-sum-pairs).
+[34. Find First and Last Position of Element in Sorted Array](https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array).
--- Solution.java
+++ Solution.java
@@ -2,30 +2,52 @@ import java.util.*;
 
 class Solution {
 
-    public static int maxOperations(int[] arr, int k) {
-        Arrays.sort(arr);
-        int i = 0;
-        int j = arr.length - 1;
-        int op = 0;
-        
-        while (i < j) {
-            if (arr[i] + arr[j] == k) {
-                i++;
-                j--;
-                op++;
-            } else if (arr[i] + arr[j] < k) {
-                i++;
-            } else if (arr[i] + arr[j] > k) {
-                j--;
+    public static int[] searchRange(int[] nums, int target) {
+        int[] result = {-1, -1};
+        int left = 0;
+        int right = nums.length - 1;
+
+        if (nums.length == 0)
+            return result;
+
+        // Binary search to find the first occurrence of target
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] == target) {
+                result[0] = mid;
+                right = mid - 1; // Continue searching on the left side
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
             }
         }
-        
-        return op;
+
+        // Reset left and right pointers
+        left = 0;
+        right = nums.length - 1;
+
+        // Binary search to find the last occurrence of target
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] == target) {
+                result[1] = mid;
+                left = mid + 1; // Continue searching on the right side
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return result;
     }
     
     public static void main(String[] args) {
-        int[] nums = { 1,2,3,4 };
-        int k = 5;
-        System.out.println( maxOperations(nums, k) );
+        int[] nums = { 5,7,7,8,8,10 };
+        int target = 8;
+        System.out.println( searchRange(nums, target) );
     }
 }
