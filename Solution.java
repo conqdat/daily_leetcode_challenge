bb06880ff9df080da6acf25da6154b974770b54f
Dat Tran <khiemtran21012000@gmail.com>
2023-10-12 10:59:30 +0700

    add optimized solution

--- Solution.java
+++ Solution.java
@@ -3,17 +3,18 @@ import java.util.*;
 
 class Solution {
     public static boolean uniqueOccurrences(int[] arr) {
-        HashMap<Integer, Integer> map = new HashMap<>();
-        Set<Integer> set = new HashSet<>();
-        
-        for(int i = 0; i < arr.length; i++) {
-            map.put(arr[i],  map.getOrDefault(arr[i], 0) + 1);
-        }
-
-        for(var entry : map.entrySet()) {
-            set.add(entry.getValue());
-        }
-        return map.size() == set.size();
+                HashMap<Integer, Integer> map = new HashMap<>();
+                
+                for (int i = 0; i < arr.length; i++) {
+                    map.put(arr[i], map.getOrDefault(arr[i], 0) + 1);
+                }
+                Set<Integer> seen = new HashSet<>();
+                for (int count : map.values()) {
+                    if (!seen.add(count)) {
+                        return false; // If a count is not unique, return false.
+                    }
+                }
+                return true; // All counts are unique.
     }
 
     public static void main(String[] args) {
