fe90f3bc886bdf7e94ac0eca996a6045120fddfa
DatTran <trancongdat.datct@gmail.com>
2023-11-07 23:56:37 +0700

    add code

--- Solution.java
+++ Solution.java
@@ -24,10 +24,50 @@ class Solution {
         return result;
     }
 
+    public static int pivotIndex(int[] nums) {
+        int leftSum = 0;
+        int total = 0;
+        for(int i = 0; i < nums.length; i++) {
+            total += nums[i];
+        }
+        for(int i = 0; i < nums.length; i++) {
+            int rightSum = total - nums[i] - leftSum;
+            if(leftSum == rightSum) {
+                return i;
+            } 
+            leftSum += nums[i];
+        }
+
+        return -1;
+    }
+
     public static void main(String[] args) {
+
+        int[] nums = {1,7,3,6,5,6};
+
+        System.out.println( pivotIndex(nums) );
+
+
+        System.out.println("-----------------------");
+
         int n = 5;
 
-        System.out.println( countBits(n) );
+        // System.out.println( countBits(n) );
 
+
+        // Right shift:     System.out.println( 80 >> 3 ); // 80 / 2^3 = 80  
+        // Left shift:      System.out.println( 2 << 5 ); // 2 * 2^5 = 64
+
+
+        // System.out.println( 3 | (1 << 3) );
+
+
+        System.out.println( 4 & 7);
+        System.out.println( 4 | 7);
+        System.out.println(4 ^ 7);
+
+        System.out.println("===================");
+        System.out.println(1024 | (1 << 2)); // 1020 | 1 * 2^2
+        System.out.println(1024 | (1 * 2^2));
     }
 }
