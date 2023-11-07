0790fc0fd6e7f466e02bff5e0bd653dc0d6e1b17
Dat Tran <khiemtran21012000@gmail.com>
2023-11-07 11:22:15 +0700

    add bit manipulation

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Daily Leetcode Challenge
 [2215. Find the Difference of Two Arrays](https://leetcode.com/problems/find-the-difference-of-two-arrays/description).
+
+
+[Bit manipulation](https://cp-algorithms.com/algebra/bit-manipulation.html)
--- Solution.java
+++ Solution.java
@@ -2,39 +2,20 @@ import java.util.*;
 
 class Solution {
 
-    public static List<List<Integer>> findDifference(int[] nums1, int[] nums2) {
-	    Set<Integer> s1 = new HashSet<>();
-        Set<Integer> s2 = new HashSet<>();
-
-        // output list
-        List<List<Integer>> out =  new ArrayList<>();
-        out.add(new ArrayList<Integer>());
-        out.add(new ArrayList<Integer>());
-
-        // adding the elements to the set
-        for (int i: nums1) {
-            s1.add(i);
-        }
-        for (int i: nums2) {
-            s2.add(i);
-        }
-
-        // checking and adding the elements to the output list
-        for (int i: s1) {
-            if (!s2.contains(i)) out.get(0).add(i);
-        }
-        for (int i: s2) {
-            if (!s1.contains(i)) out.get(1).add(i);
-        }
-        return out;
-    }
-
-
 
     public static void main(String[] args) {
-        int[] nums1 = {1, 2, 3};
-        int[] nums2 = {2, 4, 6};
 
-        System.out.println( findDifference(nums1, nums2) );
+        System.out.println( 8 >> 2 ); // 8 / 2^2 
+        System.out.println( 8 << 2 ); // 8 * 2*2
+
+        System.out.println("==========================");
+
+        
+        System.out.println( 2 | (1 << 2) ); // set bit 2-th to 1 |||| 010 => 110
+        System.out.println(2 ^ (1 << 3)); // flip bit 3-th  ||||   0010 => 1010
+        System.out.println(2 & ~(1 << 1)); // clear bit 1-th ||| 010 => 000  
     }
 }
+
+
+
