e6e180288713c7b4eea496bc99f5ea89a29efe3d
DatTran <trancongdat.datct@gmail.com>
2023-11-11 18:33:12 +0700

    some wrong code

--- Solution.java
+++ Solution.java
@@ -1,73 +1,48 @@
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 class Solution {
 
-    public static int[] countBits(int n) {
-        int[] result = new int[n+1];
-
-        for(int i = 0; i < result.length; i++) {
-            result[i] = sumOfNum( Integer.bitCount(i) );
+    public static int soOne(String S) {
+        int result = 0;
+        boolean isHaveA = false;
+        for (int i = S.length() - 1; i >= 0; i--) {
+            if(S.charAt(i) == 'A') {
+                isHaveA = true;
+            }
+            else if(S.charAt(i) == 'B' && isHaveA) {
+                result += 1;
+            }
         }
-        System.out.println( Arrays.toString(result) );
         return result;
     }
-    
 
-    public static int sumOfNum(int n) {
-        int result = 0;
+    public  static int solution(int[] A) {
+        int count = 0;
+        int previous = A[0];
 
-        while(n > 0) {
-            result += n % 10;
-            n = n / 10;
+        if(A.length <= 2) { return 0; }
+        
+        for (int i = 1; i < A.length - 1; i++) {
+            int current = A[i];
+            int next = A[i + 1];
+        
+            if (
+                (previous <= current && current >= next) 
+                || (previous >= current && current < next) 
+            )     
+            {
+                count++;
+                previous = current;
+            }
         }
         
-        return result;
-    }
-
-    public static int pivotIndex(int[] nums) {
-        int leftSum = 0;
-        int total = 0;
-        for(int i = 0; i < nums.length; i++) {
-            total += nums[i];
-        }
-        for(int i = 0; i < nums.length; i++) {
-            int rightSum = total - nums[i] - leftSum;
-            if(leftSum == rightSum) {
-                return i;
-            } 
-            leftSum += nums[i];
-        }
-
-        return -1;
+        return count;
     }
 
     public static void main(String[] args) {
-
-        int[] nums = {1,7,3,6,5,6};
-
-        System.out.println( pivotIndex(nums) );
-
-
-        System.out.println("-----------------------");
-
-        int n = 5;
-
-        // System.out.println( countBits(n) );
-
-
-        // Right shift:     System.out.println( 80 >> 3 ); // 80 / 2^3 = 80  
-        // Left shift:      System.out.println( 2 << 5 ); // 2 * 2^5 = 64
-
-
-        // System.out.println( 3 | (1 << 3) );
-
-
-        System.out.println( 4 & 7);
-        System.out.println( 4 | 7);
-        System.out.println(4 ^ 7);
-
-        System.out.println("===================");
-        System.out.println(1024 | (1 << 2)); // 1020 | 1 * 2^2
-        System.out.println(1024 | (1 * 2^2));
+        int arr[] = { 2,2,3,4,3,3,2,2,1,1,2,5 };
+        System.out.println( solution(arr) );
     }
 }
