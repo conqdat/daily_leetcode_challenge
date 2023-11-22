1a3455b382885e716a06593fb269fb08d21ca7f1
DatTran <trancongdat.datct@gmail.com>
2023-11-22 16:04:29 +0700

    add code

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # Daily Leetcode Challenge
 
 [1679. Max Number of K-Sum Pairs](https://leetcode.com/problems/max-number-of-k-sum-pairs).
+[643. Maximum Average Subarray I](https://leetcode.com/problems/maximum-average-subarray-i).
--- Solution.java
+++ Solution.java
@@ -25,12 +25,38 @@ class Solution {
         return count;
     }
 
+    public static double findMaxAverage(int[] nums, int k) {
+        double result = 0.0;
+    
+        // Calculate the sum of the first window of size k
+        for (int i = 0; i < k; i++) {
+            result += nums[i] / (double) k;
+        }
+    
+        // Initialize result with the average of the first window
+        double currentWindowValue = result;
+    
+        // Slide the window through the rest of the array
+        for (int i = k; i < nums.length; i++) {
+            // Update the currentWindowValue by subtracting the first element of the previous window
+            // and adding the current element to the window sum
+            currentWindowValue = (currentWindowValue * k - nums[i - k] + nums[i]) / k;
+    
+            // Update the result if the currentWindowValue is greater
+            result = Math.max(currentWindowValue, result);
+        }
+    
+        return result;
+    }
+    
 
+
+    
     public static void main(String[] args) {
-        int nums[] = { 1,2,3,4 };
-        int k = 5;
+        int nums[] = { 1,12,-5,-6,50,3 };
+        int k = 4;
 
 
-        System.out.println( maxOperations(nums, k) );
+        System.out.println( findMaxAverage(nums, k) );
     }
 }
