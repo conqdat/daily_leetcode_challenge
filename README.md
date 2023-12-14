c7ed444d0e08170b1f95e916d0a615289506b4de
DatTran <trancongdat.datct@gmail.com>
2023-12-14 14:10:37 +0700

    add something new

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 # Daily Leetcode Challenge
 
 [921. Minimum Add to Make Parentheses Valid](https://leetcode.com/problems/minimum-add-to-make-parentheses-valid).
+
+b = a[i];
+i++
+
+b = a[i++];
+
+```
+
+```
--- Solution.java
+++ Solution.java
@@ -23,8 +23,24 @@ class Solution {
 
 
     public static void main(String[] args) {
-        String s = "())";
+        // String s = "())";
+        // System.out.println( minAddToMakeValid(s) );
 
-        System.out.println( minAddToMakeValid(s) );
+        int[] nums = {1,2,3,4,4,45,54,1};
+        int a = 0, b = 0, c = 0;
+
+        for(int i = 0; i < nums.length; i++) {
+            b = nums[i];
+            System.out.println("b : "+ b);
+            i++;
+        }
+
+        System.out.println("==============");
+
+        for(int i = 0; i < nums.length; i++) {
+            c = nums[i++];
+            System.out.println("c : "+ c);
+        }
+
     }
 }
