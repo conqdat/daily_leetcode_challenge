d1a628dbd0b1e82cf666195636dbcd946a335015
DatTran <trancongdat.datct@gmail.com>
2023-12-27 23:13:00 +0700

    add code

--- Solution.java
+++ Solution.java
@@ -2,48 +2,32 @@ import java.util.*;
 
 class Solution {
 
-    public static int fibo(int n) {
-        if (n <= 1) return n;
-        return fibo(n - 1) + fibo(n - 2);
-    }
+    public static int minCost(String colors, int[] neededTime) {
+        int left = 0;
+        int right = 1;
+        int result = 0;
 
-    public static int tribonacci(int n) {
-        int[] f = new int[n+1];
-        
-        f[0] = 0;
-        f[1] = 1;
-        f[2] = 1;
-
-        for(int i = 3; i <= n; i ++) {
-            f[i] = f[i - 1] + f[i - 2] + f[i - 3];
+        while (right < neededTime.length) {
+            if (colors.charAt(left) == colors.charAt(right)) {
+                if (neededTime[left] < neededTime[right]) {
+                    result += neededTime[left];
+                    left = right;
+                } else {
+                    result += neededTime[right];
+                }
+            } else {
+                left = right;
+            }
+            right++;  // Add this line to update the 'right' pointer
         }
-        return f[n];
-    }
 
-
-    public static int climbStairs(int n) {
-        int[] result = new int[n + 1];
-
-        // Edge case
-        if(n == 1) return 1;
-        if(n == 2) return 2;
-        if(n == 3) return 3;
-
-        result[0] = 0;
-        result[1] = 1;
-        result[2] = 2;
-        result[3] = 3;
-
-        for(int i = 4; i <= n; i++) {
-            result[i] = result[i - 1] + result[i - 2];
-        }
-        return result[n];
+        return result;
     }
 
     public static void main(String[] args) {
-        int n = 10;
+        String colors = "aabaa";
+        int[] neededTime = {1, 2, 3, 4, 1};
 
-        System.out.println( climbStairs(n) );
-
+        System.out.println(minCost(colors, neededTime));
     }
 }
